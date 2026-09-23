3b6ae3144f810bab724d82b6d9074cacf34badc5
Santiago Tay <santiagotay3003@gmail.com>
2026-09-23 18:05:30 -0500

    Se añadio clase enemigo

---
+++ Promedio1 de Santiago Tay/Enemigo.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Promedio1_de_Santiago_Tay
+{
+    public class Enemigo
+    {
+        private int vida;
+        private int daño;
+
+        public Enemigo(int vida, int daño)
+        {
+            this.vida = vida;
+            this.daño = daño;
+        }
+        public void RecibirDaño(int cantidad)
+        {
+            this.vida -= cantidad;
+            if (this.vida < 0)
+            {
+                this.vida = 0;
+            }
+        }
+        public int ObtenerDaño()
+        {
+            return this.daño;
+        }
+        public bool EstaVivo()
+        {
+            return this.vida > 0;
+        }
+        public int ObtenerVida()
+        {
+            return this.vida;
+        }
+    }
+}
+
