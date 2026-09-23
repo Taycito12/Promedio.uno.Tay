88a7d6fb5e99ccf4898eacbf44eacbadbe0a397e
Santiago Tay <santiagotay3003@gmail.com>
2026-09-23 17:35:47 -0500

    Promedio 1 Santiago Tay

---
+++ Promedio1 de Santiago Tay/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Promedio1_de_Santiago_Tay
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
